fbff6fc6a8b6d3ac4917c46ebe37db5f81c84142
unknown <sivanandhini774@gmail.com>
2024-08-20 14:41:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+alert('test');
+setTimeout(()=> document.getElementsByClassName('name')[0].innerHTML='change name',3000)
+setTimeout(()=> document.getElementsByClassName('name')[1].innerHTML=' ',8000)
+alert(10+10)
+console.log('output')
